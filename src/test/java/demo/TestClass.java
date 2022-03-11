079c6ed29a867395cb6342e68a8e1f9454541131
Stanislav <kaplanst@mail.ru>
2022-03-11 17:45:59 -0500

    Start

--- src/test/java/demo/TestClass.java
+++ src/test/java/demo/TestClass.java
@@ -42,5 +42,18 @@ public class TestClass {
         driver.findElement(By.linkText("Latitude Laptops & Tablets")).click();
         driver.quit();
     }
+    @Test
+    public static void TestThree() throws InterruptedException {
+        WebDriver driver;
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://bhphotovideo.com");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//input[@id='top-search-input']")).sendKeys("Bububu");
+        driver.findElement(By.xpath("//button[@name='Top Nav-Search']")).click();
+        driver.quit();
+    }
 
 }
