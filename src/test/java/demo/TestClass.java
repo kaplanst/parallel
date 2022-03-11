719dc53dce1ac64dbec8bc0e78a02a507a76378f
Stanislav <kaplanst@mail.ru>
2022-03-11 17:33:33 -0500

    Start

--- src/test/java/demo/TestClass.java
+++ src/test/java/demo/TestClass.java
@@ -12,36 +12,34 @@ import java.util.concurrent.TimeUnit;
 
 public class TestClass {
 
-    public static WebDriver driver;
 
-    @BeforeMethod
-    public static void beforeTest() throws InterruptedException {
+    @Test
+    public static void TestOne() throws InterruptedException {
+        WebDriver driver;
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.get("https://bhphotovideo.com");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         Thread.sleep(2000);
-        driver.switchTo().defaultContent();
-
-    }
-
-    @Test
-    public static void TestOne(){
         driver.findElement(By.xpath("//input[@id='top-search-input']")).sendKeys("pen drive");
         driver.findElement(By.xpath("//button[@name='Top Nav-Search']")).click();
         driver.findElement(By.linkText("SanDisk 64GB Ultra Dual Drive Luxe USB 3.1 Flash Drive (USB Type-C / Type-A)")).click();
+        driver.quit();
     }
 
     @Test
-    public static void TestTwo(){
+    public static void TestTwo() throws InterruptedException {
+        WebDriver driver;
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://bhphotovideo.com");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        Thread.sleep(2000);
         driver.findElement(By.xpath("//input[@id='top-search-input']")).sendKeys("Dell laptop");
         driver.findElement(By.xpath("//button[@name='Top Nav-Search']")).click();
         driver.findElement(By.linkText("Latitude Laptops & Tablets")).click();
-    }
-
-    @AfterMethod
-    public static void AfterTest(){
         driver.quit();
     }
 
