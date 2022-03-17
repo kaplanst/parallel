77487d584979668eac44d813ea3ede42c8b45dd7
Stanislav <kaplanst@mail.ru>
2022-03-17 17:37:42 -0400

    Start

--- src/test/java/demo/TestClass1.java
+++ src/test/java/demo/TestClass1.java
@@ -1,16 +1,13 @@
 package demo;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
-public class TestClass {
+public class TestClass1 {
 
     @BeforeMethod
     public static void BeforeTest(){
